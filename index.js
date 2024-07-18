e0a2b612efb974dfccab763f7c03308a91c171fe
Satyanchal <62104921+iamsatyanchal@users.noreply.github.com>
2024-07-18 19:24:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const glob = require('glob');*/
 const API_TOKEN = '7476910522:AAFiun8K__9RvmrtXbq3L9GPIbDkQquH9xY';
 
 // Initialize Telegram Bot
-const bot = new TelegramBot(API_TOKEN, { polling: false });
+const bot = new TelegramBot(API_TOKEN, { polling: true });
 
 // Command: /start
 bot.onText(/\/start/, (msg) => {
@@ -25,30 +25,30 @@ bot.onText(/\/start/, (msg) => {
 bot.on('message', async (msg) => {
   const chatId = msg.chat.id;
   const messageText = msg.text;
-// if (msg.text.includes('instagram.com') == true) {
+ if (msg.text.includes('instagram.com') == true) {
     bot.sendMessage(msg.chat.id, 'Ruk chutiya Download Karke Deta hu 🥱');
   //  const shortcode = link.split('/').filter(Boolean).slice(-1)[0]; //kaam nhi aaya bad me aayega rakhe rakhiye isko
-fetch("https://v3.igdownloader.app/api/ajaxSearch", {
-  "headers": {
-    "accept": "*/*",
-    "accept-language": "en-US,en;q=0.9",
-    "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
-    "priority": "u=1, i",
-    "sec-ch-ua": "\"Not/A)Brand\";v=\"8\", \"Chromium\";v=\"126\", \"Microsoft Edge\";v=\"126\"",
-    "sec-ch-ua-mobile": "?0",
-    "sec-ch-ua-platform": "\"Windows\"",
-    "sec-fetch-dest": "empty",
-    "sec-fetch-mode": "cors",
-    "sec-fetch-site": "same-site"
-  },
-  "referrer": "https://igdownloader.app/",
-  "referrerPolicy": "strict-origin-when-cross-origin",
-  "body": `recaptchaToken=&q=${msg.text}&t=media&lang=en`,
-  "method": "POST",
-  "mode": "cors",
-  "credentials": "omit"
-}).then(res=> res.json())
+  fetch("https://v3.igdownloader.app/api/ajaxSearch", {
+    "headers": {
+      "accept": "*/*",
+      "accept-language": "en-US,en;q=0.9",
+      "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+      "priority": "u=1, i",
+      "referrer-policy": "strict-origin-when-cross-origin",
+      "sec-ch-ua": "\"Not/A)Brand\";v=\"8\", \"Chromium\";v=\"126\", \"Microsoft Edge\";v=\"126\"",
+      "sec-ch-ua-mobile": "?0",
+      "sec-ch-ua-platform": "\"Windows\"",
+      "sec-fetch-dest": "empty",
+      "sec-fetch-mode": "cors",
+      "sec-fetch-site": "same-site",
+      "Referer": "https://igdownloader.app/",
+      "Referrer-Policy": "strict-origin-when-cross-origin"
+    },
+    "body": `recaptchaToken=&q=${msg.text}&t=media&lang=en`,
+    "method": "POST"
+  }).then(res=> res.json())
 	  .then(data=> {
+      console.log(data)
                     if(data != undefined ) {
 			console.log(data)
 if(data.data != "") {
@@ -56,7 +56,7 @@ const $ = cheerio.load(data.data);
 
 // href attribute ko extract karna
 const downloadLink = $('.download-items__btn a').attr('href');
-bot.sendMessage(msg.chat.id, downloadLink, {
+bot.sendVideo(msg.chat.id, downloadLink, {
 reply_to_message_id: msg.message_id})
 }
 }
@@ -75,7 +75,7 @@ reply_to_message_id: msg.message_id})
       bot.sendMessage(chatId, 'Are code me kuch maiyaa blast hogya. developer ko pakad ke pelo @NemesisRoy');
     }
     */
-// }
+ }
 });
 /*
 // Function to download Instagram media
